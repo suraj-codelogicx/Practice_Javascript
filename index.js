daf0c4ca7d296618dff09466641d0ae9f614cb79
Suraj Biswas <suraj.biswas@codelogicx.com>
2022-07-06 09:50:04 +0530

    Changes

---
+++ index.js
@@ -0,0 +1,19 @@
+ //comment 
+ console.log('Hello World');
+ // veriable declaration
+ let name='Suraj' ;
+ console.log(name);
+ let firstName="Suraj";
+ let lastName="Biswas";
+ console.log(firstName,lastName);
+ const addressLog= "Kolkata";
+ console.log(addressLog);
+ //addressLog="INDIA";
+
+
+ //const name= 'Suraj Biswas'; //String literal 
+ let age =27 ;//Number Literal 
+ let isMarried= false; // boolean literal
+ console.log(age);
+ const sym= new Symbol() ;
+  
