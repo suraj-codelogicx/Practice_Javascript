99809d95c74af049744f8ca6a6375c93955c1a09
Suraj Biswas <suraj.biswas@codelogicx.com>
2022-07-09 12:17:00 +0530

    new

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
- //comment 
- console.log('Hello World');
- // veriable declaration
- let name='Suraj' ;
- console.log(name);
- let firstName="Suraj";
- let lastName="Biswas";
- console.log(firstName,lastName);
- const addressLog= "Kolkata";
- console.log(addressLog);
- //addressLog="INDIA";
-
-
- //const name= 'Suraj Biswas'; //String literal 
- let age =27 ;//Number Literal 
- let isMarried= false; // boolean literal
- console.log(age);
- const sym= new Symbol() ;
+function validateform(){  
+    var UserName=document.myform.name.value;  
+      
+    if (UserName==null || UserName==""){  
+      alert("Name can't be blank");  
+      return false;  
+    }
+      }  
   
